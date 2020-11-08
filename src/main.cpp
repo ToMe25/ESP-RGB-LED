a2aba7eb711b661e17ef78d40fb46fbd46a2603e
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2020-11-08 11:51:00 +0100

    Implement led handling
    improve networking

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,28 @@
 #include <regex>
 #include <sstream>
 #include <iomanip>
-#include <WiFi.h>
+#include <esp_wifi.h>
 #include <ESPAsyncWebServer.h>
 
 AsyncWebServer server(80);
 
 const char *ssid = "WIFI_SSID";
 const char *password = "WPA_PASSPHRASE";
+
+IPAddress localhost(192, 168, 2, 120);
+const IPAddress GATEWAY(192, 168, 2, 1);
+const IPAddress SUBNET(255, 255, 0, 0);
+
+const uint8_t PIN_RED = 5;
+const uint8_t PIN_GREEN = 18;
+const uint8_t PIN_BLUE = 19;
+
+const uint8_t CHANNEL_RED = 0;
+const uint8_t CHANNEL_GREEN = 1;
+const uint8_t CHANNEL_BLUE = 2;
+
+const uint8_t LED_RESOLUTION = 8;
+const uint16_t LED_RATE = 5000;
 
 const std::string HTML =
 		"<!DOCTYPE html>\n<html>"
@@ -20,7 +35,10 @@ const std::string HTML =
 				"required title=\"Submit hex value\" placeholder=\"$color\"><br>\n"
 				"<input type=\"submit\" value=\"Submit\"></form></div></center></body></html>";
 
-uint32_t color = 0xffff00ff;
+const char *HOSTNAME = "lightcontrol";
+
+uint32_t color = 0xffff00;
+uint8_t rgb[3];
 
 void on_not_found(AsyncWebServerRequest *request) {
 	request->send(404, "text/plain", "Not found");
@@ -43,6 +61,9 @@ void on_post_index(AsyncWebServerRequest *request) {
 		std::istringstream is(
 				request->getParam("color", true)->value().substring(1).c_str());
 		is >> std::hex >> color;
+		rgb[0] = (color >> 16);
+		rgb[1] = (color >> 8);
+		rgb[2] = color;
 	} else {
 		Serial.println("Received a post request missing the color value.");
 	}
@@ -52,13 +73,39 @@ void on_post_index(AsyncWebServerRequest *request) {
 void setup() {
 	Serial.begin(115200);
 
+	ledcSetup(CHANNEL_RED, LED_RATE, LED_RESOLUTION);
+	ledcSetup(CHANNEL_GREEN, LED_RATE, LED_RESOLUTION);
+	ledcSetup(CHANNEL_BLUE, LED_RATE, LED_RESOLUTION);
+
+	ledcAttachPin(PIN_RED, CHANNEL_RED);
+	ledcAttachPin(PIN_GREEN, CHANNEL_GREEN);
+	ledcAttachPin(PIN_BLUE, CHANNEL_BLUE);
+
+	rgb[0] = (color >> 16);
+	rgb[1] = (color >> 8);
+	rgb[2] = color;
+
 	WiFi.mode(WIFI_STA);
+
+	if (!WiFi.config(localhost, GATEWAY, SUBNET)) {
+		Serial.println("Configuring WiFi failed!");
+		return;
+	}
+
+	WiFi.setHostname(HOSTNAME);
+
 	WiFi.begin(ssid, password);
 	if (WiFi.waitForConnectResult() != WL_CONNECTED) {
 		Serial.println("Establishing a WiFi connection failed!");
 		return;
 	}
+
 	WiFi.enableIpV6();
+
+	localhost = WiFi.localIP();
+
+	Serial.print("IP Address: ");
+	Serial.println(localhost);
 
 	server.on("/", HTTP_GET, on_get_index);
 
@@ -70,5 +117,8 @@ void setup() {
 }
 
 void loop() {
-
+	ledcWrite(CHANNEL_RED, rgb[0]);
+	ledcWrite(CHANNEL_GREEN, rgb[1]);
+	ledcWrite(CHANNEL_BLUE, rgb[2]);
+	delay(100);
 }
